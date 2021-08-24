7970c6586531d3e24d1c098ac9e6cda0d1c956ef
p2v-aot <88936931+p2v-aot@users.noreply.github.com>
2021-08-24 16:32:26 +0100

    ChangeGraph2ToAVGPrice

--- graph3.js
+++ graph3.js
@@ -13,13 +13,17 @@
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     //Read the data
-    d3.csv("Results-Vol.csv",
+    d3.csv("Results-OI.csv",
 
     function(d){
-        return { TradingDate : d3.timeParse("%Y-%m-%d")(d.TradingDate), StrikePrice : d.StrikePrice, CallPut : d.CallPut, Volume: d.Volume, AVGPrice : d.AVGPrice }
+        return { TradingDate : d3.timeParse("%Y-%m-%d")(d.TradingDate), StrikePrice : d.StrikePrice, CallPut : d.CallPut, Volume: d.Volume, AVGPrice: d.AVGPrice }
     },
 
     function(data) {
+
+    var strike = d3.nest()
+        .key(function(d) { return d.StrikePrice})
+        .entries(data);
 
     var x = d3.scaleTime()
         .domain(d3.extent(data, function(d) { return d.TradingDate; }))
@@ -28,67 +32,29 @@
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
-    var y0 = d3.scaleLinear()
-        .domain(d3.extent(data, function(d) { return d.AVGPrice}))
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.AVGPrice; })])
         .range([ height, 0 ]);
         svg.append("g")
-            .call(d3.axisLeft(y0));
+            .call(d3.axisLeft(y));
 
-    var y1 = d3.scaleLinear()
-        .domain(d3.extent(data, function(d) { return d.Volume}))
-        .range([ height, 0 ]);
-        svg.append("g")
-            .attr("transform", "translate(0," + width + ")")
-            .call(d3.axisRight(y1));
+    var res = strike.map(function(d){ return d.key})
 
     var color = d3.scaleOrdinal()
-        .domain(["C","P"])
-        .range([ "#ff3333", "#66ff33"])
+        .domain(res)
+        .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-    var tooltip = d3.select("#my_dataviz").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-    var tipMouseover = function(d) {
-        var html  = d.TradingDate + "<br/>" +
-        "<span>" + d.CallPut + "</span><br/>" +
-        "<b>" + d.StrikePrice + "</b> Strike, <b>" + d.AVGPrice + "</b>";
-
-        tooltip.html(html)
-            .style("left", (d3.event.pageX + 15) + "px")
-            .style("top", (d3.event.pageY - 28) + "px")
-            .transition()
-            .duration(200)
-            .style("opacity", .9)
-    };
-
-    var tipMouseout = function(d) {
-        tooltip.transition()
-        .duration(300) // ms
-        .style("opacity", 0);
-    };
-
-    svg.append('g')
-        .selectAll("dot")
-        .data(data)
+    svg.selectAll(".line")
+        .data(strike)
         .enter()
-        .append("circle")
-            .attr("cx", function (d) { return x(d.TradingDate); } )
-            .attr("cy", function (d) { return y0(d.AVGPrice); } )
-            .attr("r", 5)
-            .style("fill", function (d) { return color(d.CallPut) } )
-            .on("mouseover", tipMouseover)
-            .on("mouseout", tipMouseout);
-
-    svg.append('g')
-        .selectAll("bar")
-        .data(data)
-        .enter()
-        .append("rect")
-            .attr("x", function(d) { return x(d.TradingDate); })
-            .attr("width", x.rangeBand())
-            .attr("y", function(d) { return y1(d.Volume); })
-            .attr("height", function(d) { return height - y(d.Volume); })
-            .style("fill", function (d) { return color(d.CallPut) } );
-
+        .append("path")
+            .attr("fill", "none")
+            .attr("stroke", function(d){ return color(d.key) })
+            .attr("stroke-width", 1.5)
+            .attr("d", function(d){
+              return d3.line()
+                .x(function(d) { return x(d.TradingDate); })
+                .y(function(d) { return y(+d.AVGPrice); })
+                (d.values)
+            })
 });
