6bece4959e8fbd5f7595215bf23b4291d1c1a96a
p2v-aot <88936931+p2v-aot@users.noreply.github.com>
2021-08-31 18:12:26 +0100

    Update graph4.js

--- graph4.js
+++ graph4.js
@@ -1,45 +1,4 @@
 
-
-function responsivefy(svg) {
-    // container will be the DOM element
-    // that the svg is appended to
-    // we then measure the container
-    // and find its aspect ratio
-    const container = d3.select(svg.node().parentNode),
-        width = parseInt(svg.style('width'), 10),
-        height = parseInt(svg.style('height'), 10),
-        aspect = width / height;
-   
-    // set viewBox attribute to the initial size
-    // control scaling with preserveAspectRatio
-    // resize svg on inital page load
-    svg.attr('viewBox', `0 0 ${width} ${height}`)
-        .attr('preserveAspectRatio', 'xMinYMid')
-        .call(resize);
-   
-    // add a listener so the chart will be resized
-    // when the window resizes
-    // multiple listeners for the same event type
-    // requires a namespace, i.e., 'click.foo'
-    // api docs: https://goo.gl/F3ZCFr
-    d3.select(window).on(
-        'resize.' + container.attr('id'), 
-        resize
-    );
-   
-    // this is the code that resizes the chart
-    // it will be called on load
-    // and in response to window resizes
-    // gets the width of the container
-    // and resizes the svg to fill it
-    // while maintaining a consistent aspect ratio
-    function resize() {
-        const w = parseInt(container.style('width'));
-        svg.attr('width', w);
-        svg.attr('height', Math.round(w / aspect));
-    }
-  }
-
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
   width = 2024 - margin.left - margin.right,
   height = 1024 - margin.top - margin.bottom;
@@ -80,3 +39,43 @@ d3.csv("Results-Vol-P-Neg.csv", function(data) {
       .style("fill", "#69b3a2")
 
 })
+
+function responsivefy(svg) {
+    // container will be the DOM element
+    // that the svg is appended to
+    // we then measure the container
+    // and find its aspect ratio
+    const container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style('width'), 10),
+        height = parseInt(svg.style('height'), 10),
+        aspect = width / height;
+   
+    // set viewBox attribute to the initial size
+    // control scaling with preserveAspectRatio
+    // resize svg on inital page load
+    svg.attr('viewBox', `0 0 ${width} ${height}`)
+        .attr('preserveAspectRatio', 'xMinYMid')
+        .call(resize);
+   
+    // add a listener so the chart will be resized
+    // when the window resizes
+    // multiple listeners for the same event type
+    // requires a namespace, i.e., 'click.foo'
+    // api docs: https://goo.gl/F3ZCFr
+    d3.select(window).on(
+        'resize.' + container.attr('id'), 
+        resize
+    );
+   
+    // this is the code that resizes the chart
+    // it will be called on load
+    // and in response to window resizes
+    // gets the width of the container
+    // and resizes the svg to fill it
+    // while maintaining a consistent aspect ratio
+    function resize() {
+        const w = parseInt(container.style('width'));
+        svg.attr('width', w);
+        svg.attr('height', Math.round(w / aspect));
+    }
+  }
