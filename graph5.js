bb7d624272b9187d4689b0c3096c87436de77226
p2v-aot <88936931+p2v-aot@users.noreply.github.com>
2021-09-01 15:34:08 +0100

    Update graph5.js

--- graph5.js
+++ graph5.js
@@ -19,7 +19,19 @@
     },
 
     function(data) {
-        print((d3.extent(data, function(d) { return d.TradingDate; })))
+        var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.TradingDate; }))
+        .range([ 0, width ])
+        .nice();
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        var y = d3.scaleLinear()
+            .domain([-100, 100])
+            .range([ height, 0 ]);
+            svg.append("g")
+                .call(d3.axisLeft(y));
     });
 
     
