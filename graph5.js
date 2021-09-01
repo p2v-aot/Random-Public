4e6ffb7584fee67965276f425d869eb457dcbe09
p2v-aot <88936931+p2v-aot@users.noreply.github.com>
2021-09-01 15:29:34 +0100

    Update graph5.js

--- graph5.js
+++ graph5.js
@@ -21,3 +21,26 @@
     function(data) {
         console.log(d3.extent(data, function(d) { return d.TradingDate; }))
     });
+
+    
+    function responsivefy(svg) {
+        const container = d3.select(svg.node().parentNode),
+            width = parseInt(svg.style('width'), 10),
+            height = parseInt(svg.style('height'), 10),
+            aspect = width / height;
+
+        svg.attr('viewBox', `0 0 ${width} ${height}`)
+            .attr('preserveAspectRatio', 'xMinYMid')
+            .call(resize);
+
+        d3.select(window).on(
+            'resize.' + container.attr('id'), 
+            resize
+        );
+
+        function resize() {
+            const w = parseInt(container.style('width'));
+            svg.attr('width', w);
+            svg.attr('height', Math.round(w / aspect));
+        }
+    }
